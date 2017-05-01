e12912dc081e9ef407cc761025d91d8b52ad7417
Dinal Herath <dinal.bing@gmail.com>
2017-05-01 14:02:39 +0530

    Intro-Java L2-Objects vid 43-44

---
+++ Intro_to_Java_programming/2_Objects/CopyNumber.java
@@ -0,0 +1,9 @@
+public class CopyNumber{
+	public static void main(String[] args) {
+		int luckyNumber = 13;
+		int luckyNumber2 = luckyNumber;
+		luckyNumber2 = 12;
+		System.out.println("luckyNumber -> "+luckyNumber+" luckyNumber2->"+luckyNumber2);
+	}
+	
+}
---
+++ Intro_to_Java_programming/2_Objects/CopyStrings.java
@@ -0,0 +1,10 @@
+public class CopyStrings{
+	public static void main(String[] args) {
+		String greeting = "Hello,World!"	;
+		String greeting2 = greeting;
+		greeting2.toUpperCase();
+		System.out.println("greeting 1 ->"+greeting);
+		System.out.println("greeting2 -> "+greeting2);
+		//there are no mutators of strings
+	}
+}
