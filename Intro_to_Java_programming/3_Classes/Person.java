498dd7dee0423aa421dc94b2612bea86e5b98e72
Dinal Herath <dinal.bing@gmail.com>
2017-05-01 20:52:40 +0530

    Intro-java L3 added constructor for Person.java

--- Intro_to_Java_programming/3_Classes/Person.java
+++ Intro_to_Java_programming/3_Classes/Person.java
@@ -1,8 +1,15 @@
-public class Person
-{
+public class Person{
+	//-----------variables-------------
 	private String name;
 	private String friends;
 
+	//-----------Constructor-----------
+	public Person(String aName){
+		name = aName;
+		friends="";
+	}
+
+	//-----------Method----------------
 	public void addFriend(Person friend)
 	{
 		friends += friend.name + " ";
@@ -11,5 +18,9 @@ public class Person
 	public void unfriend(Person nonFriend)
 	{
 		friends = friends.replace(nonFriend.name+" ","");
+	}
+
+	public String getfriends(){
+		return friends;
 	}
 }
