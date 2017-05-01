d3888f554dbb14f4fe1559137c84d4373e97c639
Dinal Herath <dinal.bing@gmail.com>
2017-05-01 14:33:20 +0530

    Intr-Java Problem Set 1 q1-2

---
+++ Intro_to_Java_programming/ProblemSet_1/Quiz1.java
@@ -0,0 +1,6 @@
+public class Quiz1{
+	public static void main(String[] args) {
+		double cost = 12.51;
+		System.out.println("Cost ->"+cost);
+	}
+}
---
+++ Intro_to_Java_programming/ProblemSet_1/Quiz2.java
@@ -0,0 +1,6 @@
+public class Quiz2 {
+	public static void main(String[] args) {
+		String course = "Udacity";
+		System.out.println(course);
+	}
+}
