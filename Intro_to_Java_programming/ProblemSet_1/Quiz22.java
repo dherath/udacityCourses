d70debcac15b20e5a16ecf3dd3fbd4ebbdbb0faa
Dinal Herath <dinal.bing@gmail.com>
2017-05-01 15:17:30 +0530

    Intro-Java Problem Set 1 q22

---
+++ Intro_to_Java_programming/ProblemSet_1/Quiz22.java
@@ -0,0 +1,9 @@
+// Name in Box
+public class Quiz22{
+	public static void main(String[] args) {
+		String name ="Udacity";
+		System.out.println("+-------+");
+		System.out.println("|"+name+"|");
+		System.out.println("+-------+");
+	}
+}
