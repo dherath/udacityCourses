17881c077be6af486fc96d3f147b9f5c55486231
Dinal Herath <dinal.bing@gmail.com>
2017-05-01 13:47:35 +0530

    Intro-Java L2-Objects vid 34 - Accessor/Mutator

---
+++ Intro_to_Java_programming/2_Objects/HelloWorldMutator.java
@@ -0,0 +1,7 @@
+public class HelloWorldMutator{
+	public static void main(String args[]){
+		String greeting = "Hello";
+		System.out.println(greeting.replace("H","J"));
+		System.out.println(greeting);
+	}
+}
