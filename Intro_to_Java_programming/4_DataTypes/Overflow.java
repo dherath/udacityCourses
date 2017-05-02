887f62dbb5a21fb8c7dfdd94b429c824ee02b083
Dinal Herath <dinal.bing@gmail.com>
2017-05-02 13:45:14 +0530

    Intro-Java L4-data types v4-Overflow

---
+++ Intro_to_Java_programming/4_DataTypes/Overflow.java
@@ -0,0 +1,8 @@
+public class Overflow{
+	public static void main(String[] args) {
+		double million = 1000000.0;
+		double mystery = million*million;
+		System.out.println(mystery);
+		// max int - 2147483647
+	}
+}
