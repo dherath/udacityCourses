6f49c14d09b96a76952866d405a5ec8fbdf5b326
Dinal Herath <dinal.bing@gmail.com>
2017-05-03 18:34:49 +0530

    Intro-Java L4 v8

---
+++ Intro_to_Java_programming/4_DataTypes/ArithmaticOperators.java
@@ -0,0 +1,16 @@
+public class ArithmaticOperators{
+	public static void main(String[] args) {
+		int x =3;
+		int y =4;
+		int xy = 11;
+		int cos = 5;
+
+		double a = ((x+1*4)*(2+y));
+		double b = x + y/2 +xy;
+		double c = (4+cos)/2;
+
+		System.out.println("a->"+a);
+		System.out.println("b->"+b);
+		System.out.println("c->"+c);
+	}
+}
