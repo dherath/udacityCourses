cc1e938cb6da173b462aa77a03b2d1645c933713
Dinal Herath <dinal.bing@gmail.com>
2017-05-12 19:37:53 +0530

    Intro-Algo L1 added time(n) t o countdown.py

--- Intro_to_Algorithms/1_SN_MagicTrick/countdown.py
+++ Intro_to_Algorithms/1_SN_MagicTrick/countdown.py
@@ -1,10 +1,14 @@
-# Measuring time as a funciton of input
+import math
+
+def time(n):
+	steps = 3 + 2*math.ciel(n/5)
+    return steps
 
 def countdown(x):
-	y=0
-	while x > 0 :
-		x = x - 5
-		y = y +1
-	print y
+    y = 0
+    while x > 0:
+        x = x - 5
+        y = y + 1
+    print y
 
 print countdown(50)
