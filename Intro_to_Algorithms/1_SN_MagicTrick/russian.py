2c99286baffc3c31c4c0f72e2951dd21b14782b9
Dinal Herath <dinal.bing@gmail.com>
2017-05-13 22:35:20 +0530

    Intro-Algo L1 added recursive_russian to russian.py

--- Intro_to_Algorithms/1_SN_MagicTrick/russian.py
+++ Intro_to_Algorithms/1_SN_MagicTrick/russian.py
@@ -8,3 +8,13 @@ def  russian(a,b):
 		y=y<<1
 		x=x>>1
 	return z
+
+# recursive algorithm
+
+def rec_russian(a,b):
+	if a==0: return 0
+	elif a%2 ==0 : return 2*rec_russian(a/2,b)
+	else  : return b+2*rec_russian((a-1)/2,b)
+
+print "russian(22,11) = ",russian(22,11)
+print "recursive russian(22,11) = ",rec_russian(22,11)
