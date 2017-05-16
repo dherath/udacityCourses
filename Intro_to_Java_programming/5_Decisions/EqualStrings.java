877c188f0113decba6416c5d264ba465568bdc9d
Dinal Herath <dinal.bing@gmail.com>
2017-05-16 17:39:48 +0530

    Intro-Java L5.2 EqualStrings.java

---
+++ Intro_to_Java_programming/5_Decisions/EqualStrings.java
@@ -0,0 +1,13 @@
+public class EqualStrings{
+	public static void main(String[] args) {
+		String first = "Uda";
+		String second = "city";
+		String firstAndSecond = first+second;
+		String third = "Udacity";
+		if(firstAndSecond == third){// == checks the location also in addition to content
+			System.out.println("They are the same");
+		}else{
+			System.out.println("firstAndSecond is "+firstAndSecond);
+		}
+	}
+}
