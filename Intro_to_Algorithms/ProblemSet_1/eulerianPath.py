f660ab9ed7afbb1e569cd5e338bd45318b61d8e7
Dinal Herath <dinal.bing@gmail.com>
2017-05-16 09:41:39 +0530

    Intro-Algo ProblemSet1 eularian path fixed-with values printed

--- Intro_to_Algorithms/ProblemSet_1/eulerianPath.py
+++ Intro_to_Algorithms/ProblemSet_1/eulerianPath.py
@@ -8,27 +8,41 @@
 # For example, if the input graph was
 # [(1, 2), (2, 3), (3, 1)]
 # A possible Eulerian tour would be [1, 2, 3, 1]
+import random
+
 
 def find_eulerian_tour(graph):
     # your code here
-    path =[]
-    initial= graph[0]
-    start = initial[0]
-    end = initial[1]
-    path.append(start)
-    path.append(end)
-    graph.remove(initial)
-    while start !=end :
-    	for pair in graph :
-			if pair[0]==end:
-				end=pair[1]
-				path.append(end)
-				graph.remove(pair)
-			elif pair[1]==end:
-				end=pair[0]
-				path.append(end) 	
-				graph.remove(pair)
-    return path
+	path =[]
+	start = graph[0][0]
+	end = graph[0][1]
+	path.append(start)
+	path.append(end)
+	graph.remove(graph[0])
+
+	while len(graph)>1:
+		test =[pair for pair in graph if pair[1]==start or pair[0]==start]
+		if len(test)==1:
+			graph.remove(test[0])    	
+		print "path = ",path
+		print "length of graph = ",len(graph)
+		print "end index = ",end, "  graph= ", graph
+		options = [pair for pair in graph if pair[1]==end or pair[0]==end]
+		print "options = ",options
+
+		pair = random.sample(options,1)
+		print "pair = ",pair
+		print "---------------------------------------"
+		graph.remove(pair[0])
+		if pair[0][0]==end:
+			end=pair[0][1]
+			path.append(end)
+		elif pair[0][1]==end:
+			end=pair[0][0]
+			path.append(end)
+		#print len(graph)
+		path.append(start)
+	return path
 
 # test cases
 
@@ -39,14 +53,8 @@ case4 = [(0, 1), (1, 5), (1, 7), (4, 5),(4, 8), (1, 6), (3, 7), (5, 9),(2, 4), (
 case5 =[(8, 16), (8, 18), (16, 17), (18, 19),(3, 17), (13, 17), (5, 13),(3, 4), (0, 18), (3, 14), (11, 14),
 	(1, 8), (1, 9), (4, 12), (2, 19),(1, 10), (7, 9), (13, 15),(6, 12), (0, 1), (2, 11), (3, 18), (5, 6), (7, 15), (8, 13), (10, 17)]
 
-
-
 print find_eulerian_tour(case1)
 print find_eulerian_tour(case2)
 print find_eulerian_tour(case3)
-print find_eulerian_tour(case4)
+print find_eulerian_tour(case4) 
 print find_eulerian_tour(case5)
-
-
-
-
