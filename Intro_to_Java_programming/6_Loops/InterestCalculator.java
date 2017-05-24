6bed21c9be04243430317580d2ee619f6f8c4441
Dinal Herath <dinal.bing@gmail.com>
2017-05-24 18:32:36 +0530

    Intro-Java L6 v3

---
+++ Intro_to_Java_programming/6_Loops/InterestCalculator.java
@@ -0,0 +1,16 @@
+public class InterestCalculator
+{
+    public static void main(String[] args)
+    {
+        double balance = 100;
+        int year = 0;
+
+        while (balance < 1000000)
+        {
+            double interest = balance * .01;
+            balance = balance + interest;
+            year++;
+        }
+        System.out.println("It will take " + year + " years.");
+    }
+}
