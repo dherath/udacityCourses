59adea9f33230c9d35541583b2b3c561bcd23b14
Dinal Herath <dinal.bing@gmail.com>
2017-05-27 09:35:35 +0530

    Intro-Java L6 Decoder.java

---
+++ Intro_to_Java_programming/6_Loops/Twitterizer.java
@@ -0,0 +1,14 @@
+// Bluej project: twitter
+//  TODO: Write code for the shorten(String longPost) method.
+public class Twitterizer
+{
+    /**
+     * Shortens and returns long posts by removing vowels
+     * @param longPost the post whos vowels need to be removed
+     * @return a version of the post without vowels
+     */
+    public String shorten(String longPost)
+    {
+
+    }
+}
