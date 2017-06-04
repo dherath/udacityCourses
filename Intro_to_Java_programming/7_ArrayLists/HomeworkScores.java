992587deacfa48ccc25b31cdb6d7d13febc7b07f
Dinal Herath <dinal.bing@gmail.com>
2017-06-04 12:07:22 +0530

    Intro-Java HomeworkScores.java added removeLowest()

--- Intro_to_Java_programming/7_ArrayLists/HomeworkScores.java
+++ Intro_to_Java_programming/7_ArrayLists/HomeworkScores.java
@@ -72,6 +72,80 @@ public class HomeworkScores
         return avgVal;
 	
     }
+
+    /**
+     * Removes the lowest score.
+     */
+    public void removeLowest()
+    {
+        // YOUR CODE HERE
+        // Remove the lowest score.
+        // You can use any of the other methods in this class.
+	double lowestVal = this.lowScore();
+	int lowestIndex = this.find(lowestVal);
+	this.remove(lowestIndex);
+    }
+
+    /**
+     * Finds the first occurrence of the given score.
+     * @param searchedValue the score to look for
+     * @return the position of scoreToFind, -1 if it's not there
+     */
+    public int find(double searchedValue)
+    {
+        int pos = 0;
+        boolean found = false;
+        while (pos < currentSize && !found)
+        {
+            if (scores[pos] == searchedValue)
+            {
+                found = true;
+            }
+            else
+            {
+                pos++;
+            }
+        }
+        if (found)
+        {
+            return pos;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+
+    /**
+     * Returns the lowest of the scores.
+     * @return the lowest score
+     */
+    public double lowScore()
+    {
+        double lowestSoFar = scores[0];
+        for (int i = 1; i < currentSize; i++)
+        {
+            if (scores[i] < lowestSoFar)
+            {
+                lowestSoFar = scores[i];
+            }
+        }
+        return lowestSoFar;
+    }
+
+    /**
+     * Removes the score at the given position.
+     * @param pos the position of the value to remove.
+     */
+    public void remove(int pos)
+    {
+        for (int i = pos + 1; i < currentSize; i++)
+        {
+            scores[i - 1] = scores[i];
+        }
+        currentSize--;
+    }
+
     
     /**
      * Builds an easy-to-read description of the scores in the object.
