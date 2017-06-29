bf0086d438fcb7789116d3187002adab9ef58740
Dinal Herath <dinal.bing@gmail.com>
2017-06-29 11:31:18 +0530

    Intro-Java L9 Interfaces v 4

---
+++ Intro_to_Java_programming/9_Interfaces/Scene.java
@@ -0,0 +1,41 @@
+// BlueJ project: lesson9/scene2
+
+import java.util.ArrayList;
+
+public class Scene
+{
+    public static void main(String[] args)
+    {
+        // TODO: Change to an ArrayList<Drawable>
+
+	ArrayList<Drawable> elements = new ArrayList<Drawable>();
+        // ArrayList<Car> cars = new ArrayList<Car>();
+        elements.add(new Car(100, 300));
+        elements.add(new Car(250, 300));
+        elements.add(new Car(250, 350));
+
+        // ArrayList<House> houses = new ArrayList<House>();
+        elements.add(new House(100, 150, 100, 100));
+        elements.add(new House(250, 100, 150, 150));
+
+        // ArrayList<Dog> dogs = new ArrayList<Dog>();
+        elements.add(new Dog(200, 100, "Fido.jpg"));
+        elements.add(new Dog(100, 400, "Rex.jpg"));
+        elements.add(new Dog(200, 400, "Lucky.jpg"));
+
+        // // TODO: Change to a single loop
+        // for (Car c : cars)
+        // {
+        //     c.draw();
+        // }
+        // for (House h : houses)
+        // {
+        //     h.draw();
+        // }
+        // for (Dog d : dogs)
+        // {
+        //     d.draw();
+        // }
+	for (Drawable d : elements) { d.draw(); }
+    }
+}
