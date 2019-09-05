2b543dee6a4af0cb90e8cb59cd017cf036bcc82d
Dinal Herath <dinal.bing@gmail.com>
2019-09-05 17:49:42 -0400

    fixed colr channel issue with MNIST example

--- Intro_PyTorch/1_DL_with_PyTorch/2_mnist_example.py
+++ Intro_PyTorch/1_DL_with_PyTorch/2_mnist_example.py
@@ -7,7 +7,7 @@ from torchvision import datasets, transforms
 
 # defines a transform to normalize data
 transform = transforms.Compose([transforms.ToTensor(),
-                                transforms.Normalize((0.5,0.5,0.5),(0.5,0.5,0.5))
+                                transforms.Normalize((0.5,),(0.5,))
 ])
 
 # obtaining the MNIST datasets
@@ -15,5 +15,13 @@ transform = transforms.Compose([transforms.ToTensor(),
 trainset = datasets.MNIST('MNIST_data/',download=True,train=True,transform=transform)
 trainloader = torch.utils.data.DataLoader(trainset,batch_size=64,shuffle=True)
 
+dataiter = iter(trainloader)
+images, labels = dataiter.next()
+#---------------------------------------
+
+def activation(x):
+    return 1/(1+torch.exp(-x))
+
+# flatten the input image
 
 
