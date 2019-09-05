bd1e8236c41d28c58785bfb4d4c79caf3a98d73d
Dinal Herath <dinal.bing@gmail.com>
2019-09-05 17:38:50 -0400

    started MNIST example

---
+++ Intro_PyTorch/1_DL_with_PyTorch/2_mnist_example.py
@@ -0,0 +1,19 @@
+import torch
+
+# usefull for computer vision problems
+from torchvision import datasets, transforms
+
+# mnist example
+
+# defines a transform to normalize data
+transform = transforms.Compose([transforms.ToTensor(),
+                                transforms.Normalize((0.5,0.5,0.5),(0.5,0.5,0.5))
+])
+
+# obtaining the MNIST datasets
+
+trainset = datasets.MNIST('MNIST_data/',download=True,train=True,transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset,batch_size=64,shuffle=True)
+
+
+
