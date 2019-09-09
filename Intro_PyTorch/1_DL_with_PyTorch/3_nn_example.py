c1592d8fd14b1d3bf75864325de39606be1ecdd1
Dinal Herath <dinal.bing@gmail.com>
2019-09-09 15:32:11 -0400

    figured out printnig weights in pytorch

--- Intro_PyTorch/1_DL_with_PyTorch/3_nn_example.py
+++ Intro_PyTorch/1_DL_with_PyTorch/3_nn_example.py
@@ -1,8 +1,11 @@
 from torch import nn
+import torch.nn.functional as F
+
+import copy
 
 class Network(nn.Module):
-    def __intit__(self):
-        super().__init__()
+    def __init__(self):
+        super(Network,self).__init__()
 
         #inputs to hidden layer linear transformation
         num_pixels = 28 * 28
@@ -11,26 +14,36 @@ class Network(nn.Module):
         self.output = nn.Linear(256,10)
 
         # sigmoid and softmax activation f()
-        self.sigmoid = nn.Sigmoid()
-        self.softmax = nn.Softmax(dim=1)
+        # commented it out since F is imported
+        #self.sigmoid = nn.Sigmoid()
+        #self.softmax = nn.Softmax(dim=1)
 
     def forward(self,x):
         # Pass the input tensor through each of the oprations
-        # the only problem is we cant see the weights
-        x = self.hidden1(x)
-        x = self.sigmoid(x)
-        x = self.hidden2(x)
-        x = self.sigmoid(x)
-        x = self.output(x)
-        x = self.softmax(x)
+        # the only problem is we cant see the weights -> maybe theer is a way
+        #-- method 1 -----------
+        #x = self.hidden1(x)
+        #x = self.sigmoid(x)
+        #x = self.hidden2(x)
+        #x = self.sigmoid(x)
+        #x = self.output(x)
+        #x = self.softmax(x)
 
+        #-- method 2 ----------- more concise
+        x = F.sigmoid(self.hidden1(x))
+        x = F.sigmoid(self.hidden2(x))
+        x = F.softmax(self.output(x), dim=1)
+        
         return x
-
-    
-
 
 #---------------------------------------------------------
 # Testing Phase
 
-#model = Network()
-#print(model)
+# we typically use relu : apprently trains faster
+model = Network()
+
+# also try the deepcopy() thing if needed
+# visualization did not work
+init_weight_size = model.output.weight.data.shape
+init_weight = model.output.weight.data.numpy()
+print("init_weight: ",init_weight)
